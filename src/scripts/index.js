76e1db63f22a1ce5d5843a5d3caa7bc95a3e031d
Claudinei Bido <bido.c@live.com>
2020-04-22 13:34:30 +0200

    working on it

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,17 @@
 const elements = {
   button: document.querySelector(".tasks-list__btn"),
   userInput: document.querySelector(".tasks-list__input"),
-  searchResList: document.querySelector(".results__list"),
+  tasksList: document.getElementById("results__list"),
+};
+
+const colorGenerator = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * letters.length)];
+  }
+  color += "50";
+  return color;
 };
 
 const addItem = () => {
@@ -9,9 +19,20 @@ const addItem = () => {
   let userInput = elements.userInput.value;
   //console.log("userInput", userInput);
 
-  const markUp = `<li>${userInput}</li>`;
-  elements.searchResList.insertAdjacentHTML("beforebegin", markUp);
+  if (userInput.length == 0) {
+    document.querySelector(".alert").classList.remove("hidden");
+  } else {
+    document.querySelector(".alert").classList.add("hidden");
 
+    const even = `<li>${userInput}</li>`;
+    const odd = `<li style='background:${colorGenerator()}'>${userInput}</li>`;
+
+    if (elements.tasksList.children.length % 2 !== 0) {
+      elements.tasksList.insertAdjacentHTML("beforeend", odd);
+    } else {
+      elements.tasksList.insertAdjacentHTML("beforeend", even);
+    }
+  }
   //clean the field input
   elements.userInput.value = "";
 };
